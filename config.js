0755a125947566dee091949c8c0f1158fcd7e670
infinitycoding222 <58121837+infinitycoding222@users.noreply.github.com>
2021-03-20 19:12:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.config = {
-    token: "",
-    prefix: "",
-    purp: "Custom Commands Made by Fire Development LCL", // do not change that for credits
-    color: "",
-    owners: [] // owners must be string
+    token: "ODIyODc5MzkyNTA1MTM1MTQ2.YFYsJw.PVI51Wa-u4td1lyP1aMNSPB_zCM",
+    prefix: "asm.,
+    purp: "Custom Commands Made by Vulcan Development LCL", // do not change that for credits
+    color: "BLACK",
+    owners: ["473276250815856650"] // owners must be string
 }
