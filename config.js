611bab8bf4cb3f2a50292324435aa0c223b1ec22
infinitycoding222 <58121837+infinitycoding222@users.noreply.github.com>
2021-03-20 19:30:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.config = {
-    token: "ODIyODc5MzkyNTA1MTM1MTQ2.YFYsJw.PVI51Wa-u4td1lyP1aMNSPB_zCM",
+    token: "",
     prefix: "asm.,
     purp: "Custom Commands Made by Vulcan Development LCL", // do not change that for credits
     color: "BLACK",
